1401c564d959a2b6fb44c77b09f5389abf9f6f2d
Lueny-cn <574425206@qq.com>
2019-08-20 14:45:17 +0800

    Create setIntervalTest.js
    get setInterval delay in Chrome

---
+++ JavaScript/setIntervalTest.js
@@ -0,0 +1,19 @@
+function getIntervalDelay(countTime = 10, wait = 500) {
+	let now = Date.now()
+	let count = countTime
+	let delayTime = 0
+	const COUNT = count
+	return new Promise( (resolve, reject) => {
+		let timer = setInterval(() => {
+		// console.log('now count:', count)
+			if (--count === 0) {
+				clearInterval(timer)
+				resolve(Date.now()- (now + wait * COUNT))
+			} else {
+				window.__delayTime = `count:${count} delay: ${Date.now()- (now + wait * (COUNT - count))}ms`
+        }
+       }, wait)
+	})
+}
+let time = await getIntervalDelay(10000, 500)
+console.log(`delay time: ${time}ms`)
